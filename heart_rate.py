e27f8cb3e9e11f139bd4a45e3604bd58eea7beae
DAV3 <domingodavid1701@gmail.com>
2025-05-11 02:57:20 +0100

    Delete heart_rate.py

--- heart_rate.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Please enter your age in: "))
-
-max_heart_rate = 220 - age
-
-lower_target_heart_rate = 0.50 * max_heart_rate
-upper_target_heart_rate = 0.85 * max_heart_rate
-
-print("Your maximum heart rate is:", max_heart_rate, "beats per minute")
-print("Your target heart rate range is between", lower_target_heart_rate, "and", upper_target_heart_rate, "beats per minute")
