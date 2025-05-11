834f3dd447cfd68aa63c3fd0f0b2b2cf86684aa0
DAV3 <domingodavid1701@gmail.com>
2025-05-11 16:42:59 +0100

    Add files via upload

---
+++ chapter2-Ex/grade.py
@@ -0,0 +1,6 @@
+grade = int(input('Enter your Grade: '))
+
+if grade >= 90:
+	print('Congratulations Your grade of', grade, 'earns you an A in this course')
+if grade > 60 and < 90:
+	print('Congratulations Your grade of', grade, 'earns you an B in this course')
---
+++ chapter2-Ex/rating.py
@@ -0,0 +1,2 @@
+rating = int(input('Enter a number between 1 and 10: '))
+#The int() function converts the string input to an integer.
