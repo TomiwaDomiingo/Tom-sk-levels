ab8c59f4dad650bfb58d351beed927434d542045
DAV3 <domingodavid1701@gmail.com>
2025-05-11 03:52:29 +0100

    Delete blast_off.py

--- blast_off.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a positive number: "))
-
-while n < 1:
-    print("Please enter a positive number:")
-    n = int(input())
-for countdown in range(n, 0, -1):
-    print(countdown)
-print("Blast Off!")
