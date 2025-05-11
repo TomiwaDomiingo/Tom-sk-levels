9a269f6e16ca3cac81f37d2785583fc43786a58a
DAV3 <domingodavid1701@gmail.com>
2025-05-11 02:58:47 +0100

    Delete hypotenus.py

--- hypotenus.py
+++
@@ -1,7 +0,0 @@
-for side1 in range(1, 21):
-for side2 in range(1, 21):
-for hypotenuse in range(1, 21):
-            # Check if the Pythagorean theorem holds
-if side12 + side22 == hypotenuse**2:
-                # Print the Pythagorean triple
-print(side1, side2, hypotenuse)
