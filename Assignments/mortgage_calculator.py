9aff7ae08a8159e604f818ee34d73b5a9844bc66
DAV3 <domingodavid1701@gmail.com>
2025-05-11 16:50:26 +0100

    Add files via upload

---
+++ Assignments/mortgage_calculator.py
@@ -0,0 +1,15 @@
+
+print("Enter  loan amount:")
+principal = float(input())
+
+print("Enter the annual interest rate:")
+interest_rate = float(input())
+
+print("Enter the loan duration in years:")
+duration = int(input())
+
+monthly_rate = interest_rate / 12
+number_of_payments = duration * 12
+monthly_payment = principal *( (monthly_rate * (1 + monthly_rate)*number_of_payments) / ((1 + monthly_rate) *number_of_payments - 1))
+
+print("Your monthly mortgage payment is: ", monthly_payment)
