b0aeb99df42aae7512632240fafe198f06c8ccd9
DAV3 <domingodavid1701@gmail.com>
2025-05-11 03:00:20 +0100

    Delete number_sq_cb.py

--- number_sq_cb.py
+++
@@ -1,6 +0,0 @@
-print("number\tsquare\tcube")
-
-for number in range(6):
-    square = number * number  #Calculate the square
-    cube = number * number * number  #Calculate the cube
-    print(f"{number}\t{square}\t{cube}")  #Print the number, square, and cube, separated by tabs
