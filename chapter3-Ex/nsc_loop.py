de5e3934437b04ba3ce0e1296ed56d69f4568971
DAV3 <domingodavid1701@gmail.com>
2025-05-14 03:13:42 +0100

    Delete chapter3-Ex/nsc_loop.py
    Had to rename it

--- chapter3-Ex/nsc_loop.py
+++
@@ -1,9 +0,0 @@
-print("number square cube")
-
-for number in range(6):
-    # Calculate the square of the number
-    square = number * number
-    # Calculate the cube of the number
-    cube = number * number * number
-    # Print the number, square, and cube in a row
-    print(number, square, cube)
