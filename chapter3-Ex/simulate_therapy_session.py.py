120eb30a7b347e60eaca674bc4f3b747b01c5178
DAV3 <domingodavid1701@gmail.com>
2025-05-14 07:47:37 +0100

    Add files via upload

---
+++ chapter3-Ex/simulate_therapy_session.py.py
@@ -0,0 +1,12 @@
+# Simulate medical diagnosis
+print("What is your problem?")
+input()  # Ignore user's input
+
+print("Have you had this problem before (yes or no)?")
+answer = input().lower()
+
+if  answer == "yes":
+    print("Well, you have it again.")
+elif answer == "no":
+    print("Well, you have it now.")
+el:  print("Please answer yes or no.")
