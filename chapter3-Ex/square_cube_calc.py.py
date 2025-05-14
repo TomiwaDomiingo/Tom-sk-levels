5831424ea3a19c169fc49cebb004ecdfbb5d0bef
DAV3 <domingodavid1701@gmail.com>
2025-05-14 07:48:54 +0100

    Add files via upload

---
+++ chapter3-Ex/square_cube_calc.py.py
@@ -0,0 +1,9 @@
+print("number square cube")
+
+for number in range(6):
+    # Calculate the square of the number
+    square = number * number
+    # Calculate the cube of the number
+    cube = number * number * number
+    # Print the number, square, and cube in a row
+    print(number, square, cube)
