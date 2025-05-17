6b07595db96f9206d82c95437e915f38699320b7
DAV3 <domingodavid1701@gmail.com>
2025-05-17 13:10:07 +0100

    Delete chapter3-Ex/palindrome.py

--- chapter3-Ex/palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(number):
-    number_str = str(number)
-    return len(number_str)== 5 and number_str == number_str[::-1]
-number = int(input('Enter a fiv3 digit number:'))
-
-if is_palindrome(number):
-    print('it is a palindrome')
-else:
-    print('it is not a palindrome')    
