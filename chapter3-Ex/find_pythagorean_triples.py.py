e72d33e6bd4892c0912b04f897b0f55fc6226a74
DAV3 <domingodavid1701@gmail.com>
2025-05-18 16:27:47 +0100

    Update find_pythagorean_triples.py.py
    rewrote it to be based off user input

--- chapter3-Ex/find_pythagorean_triples.py.py
+++ chapter3-Ex/find_pythagorean_triples.py.py
@@ -1,7 +1,8 @@
-for side1 in range(1, 21):
-for side2 in range(1, 21):
-for hypotenuse in range(1, 21):
+number_limit = int(input("Enter the limit for the range: "))
+for side1 in range(1, number_limit +1): 
+	for side2 in range(1, number_limit +1):
+		for hypotenuse in range(1, number_limit +1):
             # Check if the Pythagorean theorem holds
-if side12 + side22 == hypotenuse**2:
+			if side1 + side2 == hypotenuse**2:
                 # Print the Pythagorean triple
-print(side1, side2, hypotenuse)
+				print(side1, side2, hypotenuse)
